c6e1be9ac4d8fd263dff265cc92cbf5209d0d467
seanbang <sean@bangerterx.net>
2015-05-22 15:39:34 -0700

    Updates

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
 var main = function() {
+    $('.article').click(function() {
+    $('.article').removeClass('current');
+    $('.description').hide();
+    $(this).addClass('current');
+    $(this).children('.description').show();
+    });
+    
+    $(document).keypress(function(event) {
+        if(event.which === 111) {
+        $('.current').children('.description').toggle();
+        }
+
+        else if(event.which === 110) {
+        var currentArticle = $('.current');
+        var nextArticle = currentArticle.next();
+        
+        currentArticle.removeClass('current');
+        nextArticle.addClass('current');
+        }
+
+        else if(event.which === 112) {
+        var currentArticle = $('.current');
+        var prevArticle = currentArticle.prev();
+        
+        currentArticle.removeClass('current');
+        prevArticle.addClass('current');
+        }
+
+    });
     
 };
 
